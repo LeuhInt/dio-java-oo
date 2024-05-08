6dcae4fb2774c9093d56f1850a6136f0207fe237
LeuhInt <leonardoint.si@gmail.com>
2024-05-08 17:19:58 -0300

    criado classes e interfaces

---
+++ iPhone/src/iphone/IPhone.java
@@ -0,0 +1,52 @@
+package iphone;
+
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+import interfaces.ReprodutorMusical;
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void ligar() {
+        System.out.println("Ligando");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando Correio Voz");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo Pagina");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando Nova Aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando pagina");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando musica");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando musica");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando musica");
+    }
+}
---
+++ iPhone/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import iphone.IPhone;
+
+public class Main {
+    public static void main(String[] args) {
+        IPhone iphone = new IPhone();
+
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        iphone.selecionarMusica();
+        iphone.tocar();
+        iphone.pausar();
+        iphone.exibirPagina();
+        iphone.atualizarPagina();
+        iphone.adicionarNovaAba();
+
+    }
+}
